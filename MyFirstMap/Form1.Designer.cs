42b76790e41cddbfcabbf67cdb0620c2545a15b2
fuwei007 <fuwei007@gmail.com>
2022-07-31 18:19:38 -0500

    Init the project

---
+++ MyFirstMap/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace MyFirstMap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mapView = new ThinkGeo.UI.WinForms.MapView();
+            this.SuspendLayout();
+            // 
+            // mapView
+            // 
+            this.mapView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mapView.BackColor = System.Drawing.Color.White;
+            this.mapView.CurrentScale = 0D;
+            this.mapView.ForeColor = System.Drawing.Color.Black;
+            this.mapView.Location = new System.Drawing.Point(12, 12);
+            this.mapView.MapFocusMode = ThinkGeo.Core.MapFocusMode.Default;
+            this.mapView.MapResizeMode = ThinkGeo.Core.MapResizeMode.PreserveScale;
+            this.mapView.MaximumScale = 1.7976931348623157E+308D;
+            this.mapView.MinimumScale = 200D;
+            this.mapView.Name = "mapView";
+            this.mapView.RestrictExtent = null;
+            this.mapView.RotatedAngle = 0F;
+            this.mapView.Size = new System.Drawing.Size(1073, 606);
+            this.mapView.TabIndex = 1;
+            this.mapView.MapClick += new System.EventHandler<ThinkGeo.Core.MapClickMapViewEventArgs>(this.mapView_MapClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1097, 616);
+            this.Controls.Add(this.mapView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ThinkGeo.UI.WinForms.MapView mapView;
+    }
+}
+
---
+++ MyFirstMap/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ThinkGeo.Core;
+
+namespace MyFirstMap
+{
+    public partial class Form1 : Form
+    {
+        InMemoryFeatureLayer inMemoryFeatureLayer;
+        PopupOverlay popupOverlay;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            ProjectionConverter projectionConverter = new ProjectionConverter(4326, 900913);
+            projectionConverter.Open();
+
+
+            mapView.CurrentExtent = MaxExtents.BingMaps;
+            BingMapsOverlay bingMapsOverlay = new BingMapsOverlay("AnJQit0BPR7vlanLKgsmLy43yj_WxNMtyQJJJF7QD2OJ_STvHNy4w1CaGr3ZeI3w", BingMapsMapType.AerialWithLabels);
+            mapView.Overlays.Add(bingMapsOverlay);
+
+
+            LayerOverlay layerOverlay = new LayerOverlay();
+            inMemoryFeatureLayer = new InMemoryFeatureLayer();
+            inMemoryFeatureLayer.ZoomLevelSet.ZoomLevel01.DefaultPointStyle = new PointStyle(PointSymbolType.Circle, 12, GeoBrushes.Red, GeoPens.White);
+            inMemoryFeatureLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+
+            layerOverlay.Layers.Add(inMemoryFeatureLayer);
+            mapView.Overlays.Add(layerOverlay);
+
+
+            popupOverlay = new PopupOverlay();
+            mapView.Overlays.Add(popupOverlay);
+
+
+
+
+            string[] lines = System.IO.File.ReadAllLines("sample.csv");
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string[] parts = lines[i].Split(new char[] { ',' });
+                double longitude = double.Parse(parts[0]);
+                double latitude = double.Parse(parts[1]);
+
+                Dictionary<string, string> items = new Dictionary<string, string>();
+                items.Add("Name", parts[2]);
+                items.Add("Address", parts[3]);
+                Feature feature = new Feature(longitude, latitude, i.ToString(),  items);
+                inMemoryFeatureLayer.InternalFeatures.Add(projectionConverter.ConvertToExternalProjection(feature));
+            }
+            inMemoryFeatureLayer.Open();
+            mapView.CurrentExtent = inMemoryFeatureLayer.GetBoundingBox();
+
+        }
+
+        private void mapView_MapClick(object sender, MapClickMapViewEventArgs e)
+        {
+           popupOverlay.Popups.Clear();
+           Collection<Feature> features = inMemoryFeatureLayer.QueryTools.GetFeaturesNearestTo(e.WorldLocation, GeographyUnit.Meter, 1, ReturningColumnsType.AllColumns);
+
+
+            if (features.Count > 0)
+            {
+                var distance = MapUtil.GetScreenDistanceBetweenTwoWorldPoints(mapView.CurrentExtent, new Feature(e.WorldLocation), features[0], mapView.Width, mapView.Height);
+
+                if (distance < 15)
+                {
+                    var pop = new Popup((PointShape)features[0].GetShape())
+                    {
+                        Content =  $"Name: {features[0].ColumnValues["name"].ToString()}{Environment.NewLine}Address:{features[0].ColumnValues["address"]}"
+                    };
+                    popupOverlay.Popups.Add(pop);
+                }
+            }
+
+            popupOverlay.Refresh();
+        }
+    }
+}
